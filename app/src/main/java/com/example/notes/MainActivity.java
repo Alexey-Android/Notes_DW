1cb038b5f178d83d05816b360db893475cd60863
Alexey Alexandrov <alexei.maui@gmail.com>
2020-07-20 09:44:53 +0300

    changed MainActivity.java

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -5,16 +5,14 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.InputType;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
-import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedWriter;
@@ -32,65 +30,55 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_pin);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //getSupportActionBar().setDisplayShowHomeEnabled(true);
-        //getSupportActionBar().setTitle("Настройки");
 
         init();
     }
 
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.new_pin_menu, menu);
-        return true;
-    }*/
-
     private void init() {
 
         Button btnSave = findViewById(R.id.save);
-        final ImageButton ibtnEye = findViewById(R.id.ib_eye);
+        final TextView fourSymbols = findViewById(R.id.tv_four_symbol);
+        final ImageButton iBtnEye = findViewById(R.id.ib_eye);
         final boolean[] flag = {true};
 
         final EditText newPin = findViewById(R.id.et_new_pin);
 
         btnSave.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
 
                 final String password = newPin.getText().toString();
-                if (password.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Введите новый пин", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                boolean isPasswordWritten = writeToFile(password, passwordFileName);
-                if (isPasswordWritten) {
-                    Toast.makeText(MainActivity.this, "Пин успешно записан", Toast.LENGTH_SHORT).show();
+                if (password.length() != 4) {
+                    fourSymbols.setTextColor(getResources().getColor(R.color.colorRed));
+                    Toast.makeText(MainActivity.this, "Введите новый пин из 4-х цифр", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(MainActivity.this, "Пин не был записан", Toast.LENGTH_SHORT).show();
+                    fourSymbols.setTextColor(getResources().getColor(R.color.colorGrey));
+                    boolean isPasswordWritten = writeToFile(password, passwordFileName);
+                    if (isPasswordWritten) {
+                        Toast.makeText(MainActivity.this, "Новый пин успешно записан", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
-        ibtnEye.setOnClickListener(new View.OnClickListener() {
+        iBtnEye.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-
                 // меняем изображение на кнопке
                 if (flag[0]) {
-                    ibtnEye.setImageResource(R.drawable.ic_baseline_visibility_24);
+                    iBtnEye.setImageResource(R.drawable.ic_baseline_visibility_24);
                     // показываем пин
                     newPin.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
                 } else {
                     // возвращаем первую картинку
-                    ibtnEye.setImageResource(R.drawable.ic_baseline_visibility_off_24);
+                    iBtnEye.setImageResource(R.drawable.ic_baseline_visibility_off_24);
                     // скрываем пин
                     newPin.setTransformationMethod(PasswordTransformationMethod.getInstance());
                 }
                 flag[0] = !flag[0];
             }
         });
-
     }
 
     private boolean writeToFile(String str, String fileName) {
@@ -121,7 +109,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 //finish();
                 return true;
-
             default:
                 return super.onOptionsItemSelected(item);
         }

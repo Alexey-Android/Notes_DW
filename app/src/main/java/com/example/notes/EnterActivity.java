1c5502c4a94e1e3132c5252ab7dbd637e86771ea
Alexey Alexandrov <alexei.maui@gmail.com>
2020-07-22 23:26:07 +0300

    changed EnterActivity.java

--- app/src/main/java/com/example/notes/EnterActivity.java
+++ app/src/main/java/com/example/notes/EnterActivity.java
@@ -2,16 +2,21 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.Color;
+import android.graphics.drawable.ShapeDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class EnterActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button button_del, button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
+    private TextView tv2, tv4, tv6, tv8;
     String code = "";
     StringBuffer sb = new StringBuffer();
 
@@ -37,6 +42,11 @@ public class EnterActivity extends AppCompatActivity implements View.OnClickList
         button8 = findViewById(R.id.btn8);
         button9 = findViewById(R.id.btn9);
         button_del = findViewById(R.id.btn_del);
+
+        tv2 = findViewById(R.id.tv_2);
+        tv4 = findViewById(R.id.tv_4);
+        tv6 = findViewById(R.id.tv_6);
+        tv8 = findViewById(R.id.tv_8);
 
         button0.setOnClickListener(this);
         button1.setOnClickListener(this);
@@ -64,47 +74,96 @@ public class EnterActivity extends AppCompatActivity implements View.OnClickList
         switch (v.getId()) {
             case R.id.btn0:
                 sb.append(button0.getText().toString());
+                checkPin();
+                //tv2.setBackgroundColor(getResources().getColor(R.color.colorAccent));
                 Toast.makeText(EnterActivity.this, "Длина " + sb.length(), Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn1:
                 sb.append(button1.getText().toString());
-                Toast.makeText(EnterActivity.this, "Длина " + sb.length(), Toast.LENGTH_SHORT).show();
+                checkPin();
+                //Toast.makeText(EnterActivity.this, "Длина " + sb.length(), Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn2:
                 sb.append(button2.getText().toString());
+                checkPin();
                 Toast.makeText(EnterActivity.this, "Длина " + sb.length(), Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn3:
                 sb.append(button3.getText().toString());
+                checkPin();
                 Toast.makeText(EnterActivity.this, "Длина " + sb.length(), Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn4:
                 sb.append(button4.getText().toString());
+                checkPin();
                 Toast.makeText(EnterActivity.this, "Длина " + sb.length(), Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn5:
                 sb.append(button5.getText().toString());
+                checkPin();
                 Toast.makeText(EnterActivity.this, "Длина " + sb.length(), Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn6:
                 sb.append(button6.getText().toString());
+                checkPin();
                 Toast.makeText(EnterActivity.this, "Длина " + sb.length(), Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn7:
                 sb.append(button7.getText().toString());
+                checkPin();
                 Toast.makeText(EnterActivity.this, "Длина " + sb.length(), Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn8:
                 sb.append(button8.getText().toString());
+                checkPin();
                 Toast.makeText(EnterActivity.this, "Длина " + sb.length(), Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn9:
                 sb.append(button9.getText().toString());
+                checkPin();
                 Toast.makeText(EnterActivity.this, "Длина " + sb.length(), Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_del:
-                sb.append(button_del.getText().toString());
-                Toast.makeText(EnterActivity.this, "Длина " + sb.length(), Toast.LENGTH_SHORT).show();
+                //sb.append(button_del.getText().toString());
+                sb.deleteCharAt((sb.length() - 1));
+                checkPin();
+                Toast.makeText(EnterActivity.this, "Длина " + sb.length() + "Значение " + sb, Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    private void checkPin() {
+        switch (sb.length()) {
+            case 0:
+                tv2.setBackgroundColor(Color.GRAY);
+                tv4.setBackgroundColor(Color.GRAY);
+                tv6.setBackgroundColor(Color.GRAY);
+                tv8.setBackgroundColor(Color.GRAY);
+                break;
+            case 1:
+                tv2.setBackgroundColor(Color.YELLOW);
+                tv4.setBackgroundColor(Color.GRAY);
+                tv6.setBackgroundColor(Color.GRAY);
+                tv8.setBackgroundColor(Color.GRAY);
+                break;
+            case 2:
+
+                tv2.setBackgroundColor(Color.YELLOW);
+                tv4.setBackgroundColor(Color.YELLOW);
+                tv6.setBackgroundColor(Color.GRAY);
+                tv8.setBackgroundColor(Color.GRAY);
+                break;
+            case 3:
+                tv2.setBackgroundColor(Color.YELLOW);
+                tv4.setBackgroundColor(Color.YELLOW);
+                tv6.setBackgroundColor(Color.YELLOW);
+                tv8.setBackgroundColor(Color.GRAY);
+                break;
+            case 4:
+                tv2.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                tv4.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                tv6.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                tv8.setBackgroundColor(getResources().getColor(R.color.colorAccent));
                 break;
         }
     }
